1b217dcfa51c274db3965105c2ca1c27d282686b
Felix De Montis <felix@demont.is>
2021-08-17 19:25:04 +0200

    Initial Commit

---
+++ BaustelleLive/BaustelleLiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaustelleLiveApp.swift
+//  BaustelleLive
+//
+//  Created by Felix De Montis on 17.08.21.
+//
+
+import SwiftUI
+
+@main
+struct BaustelleLiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
