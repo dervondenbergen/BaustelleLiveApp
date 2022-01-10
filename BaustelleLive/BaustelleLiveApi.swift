8b387feaa5b096393069c16f84dcc77b34475ec6
Felix De Montis <felix.demontis@orf.at>
2022-01-10 14:13:19 +0100

    fix app crash when no callout is defined in api

--- BaustelleLive/BaustelleLiveApi.swift
+++ BaustelleLive/BaustelleLiveApi.swift
@@ -10,7 +10,7 @@ import Foundation
 struct BaustelleLiveApi: Codable {
     var li16: BaustelleLiveApiLocation!
     var li27: BaustelleLiveApiLocation
-    var callout: String
+    var callout: String?
 }
 struct BaustelleLiveApiLocation: Codable {
     var id: String
