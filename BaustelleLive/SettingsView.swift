a71968d5d22ccc33fbd928739eec6ce403cd2c77
Felix De Montis <felix.demontis@orf.at>
2022-03-30 20:21:56 +0200

    update settings screen

--- BaustelleLive/SettingsView.swift
+++ BaustelleLive/SettingsView.swift
@@ -15,9 +15,43 @@ struct SettingsView: View {
     var body: some View {
         NavigationView {
             List {
-                Toggle(isOn: $shouldReload) {
-                    Text("Bilder automatisch updaten")
+                Section(footer: Text("Wenn sich die App im Vordergrund befindet, werden zwischen 5 und 23 Uhr die Bilder alle 10 Sekunden neu geladen.")) {
+                    Toggle(isOn: $shouldReload) {
+                        Text("Bilder automatisch updaten")
+                    }
                 }
+                
+                HStack {
+                    AsyncImage(url: URL(string: "https://felix.dm/assets/images/avatar.jpg")) { image in
+                        image
+                            .resizable()
+                            .scaledToFit()
+                    } placeholder: {
+                        Color.green.opacity(0.3)
+                    }
+                    .aspectRatio(1 / 1, contentMode: .fit)
+                    .frame(maxWidth: .infinity)
+                    .cornerRadius(100)
+                    
+                    VStack(spacing: 6) {
+                        Spacer()
+                        Text("Ein Projekt von")
+                            .font(.system(size: 15))
+                        
+                        Text("Felix De Montis")
+                            .bold()
+                        
+                        Text("@dervondenbergen")
+                            .font(.system(size: 12))
+                            .italic()
+                            
+                        Spacer()
+                    }
+                    .multilineTextAlignment(.trailing)
+                    .frame(maxWidth: .infinity)
+                }
+                .listRowInsets(.zero)
+                .listRowBackground(Color.clear)
             }
             .navigationTitle("Einstellungen")
             .toolbar {
